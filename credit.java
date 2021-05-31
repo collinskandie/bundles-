f82aeeedb9806e7a2691ce0c5b18ef254192adcd
Collins Kipruto <53892938+collinskandie@users.noreply.github.com>
2021-05-31 13:15:25 +0300

    initial commit :smile:

---
+++ credit.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+import java.util.Scanner;
+
+public Main{
+    public static void main() {
+        mainMenu();
+    
+        
+    }
+    public void mainMenu(){
+
+    }
+}
